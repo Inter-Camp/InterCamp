7364de3e66cd8bc9118004cc456c7ac8f21ccf37
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-29 15:02:29 -0400

    created exit-icon, conected Link from react-router to it

---
+++ src/components/icons/exit-icon.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+export const ExitIcon = () => {
+  return <ExitToAppIcon fontSize="medium" color="primary" />;
+};
--- src/pages/quiz/quiz-page.component.jsx
+++ src/pages/quiz/quiz-page.component.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./quiz-page.styles.scss";
-import QuestionPreview from '../../components/question-preview/question-preview.component';
+import QuestionPreview from "../../components/question-preview/question-preview.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
+import { ExitIcon } from "../../components/icons/return-icon.component";
+import { Link } from "react-router-dom";
 
 const QuizPage = ({ match }) => {
   console.log(match);
   const id = match.params.quizId;
   return (
     <div className="quiz-page">
+      <div className="button-container">
+        <Link to={'/quiz'}>
+          <ExitIcon />
+        </Link>
+      </div>
       <h1>Welcome to {id.toUpperCase()} Challenge</h1>
-      <QuestionPreview quizId={id}/>
+      <QuestionPreview quizId={id} />
     </div>
   );
 };
