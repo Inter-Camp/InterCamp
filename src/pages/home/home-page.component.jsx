817c43b427b15536159c82362b4e0100aaa1a7e6
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-30 17:16:43 -0400

    reorganized components

--- src/pages/home/home-page.component.jsx
+++ src/pages/home/home-page.component.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import "./home-page.styles.scss";
-import { Link } from "react-router-dom";
-import CustomButton from "../../components/custom-button/custom-button.component";
-import { Button } from "@material-ui/core";
+import About from '../../components/about/about.component';
+import SignIn from "../../components/homepage.sign-in/sign-in.component";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>Home Page</h2>
-      <Link className="go-to-quiz" to="/quiz">
-        <CustomButton>Go To Quiz</CustomButton>
-      </Link>
-      <Link className="option" to="/quiz">
-        <Button color="primary" variant="outlined">
-          CONTINUE AS GUEST
-        </Button>
-      </Link>
+      <About></About>
+      <SignIn />
     </div>
   );
 };
