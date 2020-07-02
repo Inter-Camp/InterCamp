2a2a3fa37715e8f911dbc3753f2c5a8db045a46d
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-02 10:42:36 -0400

    Redirect to the right page depending on the state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 
 import ChooseQuizPage from "./pages/choose-quiz/choose-quiz.component";
 import HomePage from "./pages/home/home-page.component";
@@ -8,39 +8,32 @@ import QuizPage from "./pages/quiz/quiz-page.component";
 import Header from "./components/header/header.component";
 import { auth } from "./firebase/firebas.utils";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
-
-  unsubscibeFromAuth = null;
-
-  componentDidMount() {
-    this.unsubscibeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+function App() {
+  const history = useHistory();
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const unsubscibeFromAuth = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      if(user) {
+        history.push('/quiz');
+      } else {
+        history.push('/');
+      }
     });
-  }
-
-  componentWillUnmount() {
-    this.unsubscibeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/quiz" component={ChooseQuizPage} />
-          <Route path="/quiz/:quizId" component={QuizPage} />
-        </Switch>
-      </div>
-    );
-  }
+    return () => {
+      unsubscibeFromAuth();
+    };
+  }, []);
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/quiz" component={ChooseQuizPage} />
+        <Route path="/quiz/:quizId" component={QuizPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
