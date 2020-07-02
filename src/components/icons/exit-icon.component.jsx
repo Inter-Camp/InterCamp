d9df3aee83230122a4154b6206d7b0cc2c797242
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-02 14:52:34 -0400

    fixed icon fontSize

--- src/components/icons/exit-icon.component.jsx
+++ src/components/icons/exit-icon.component.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 export const ExitIcon = () => {
-  return <ExitToAppIcon fontSize="medium" color="primary" />;
+  return <ExitToAppIcon  color="primary" />;
 };
