37c970dda1dd0cbbfd5499e3a62ff4c878c97913
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-06 15:07:16 -0400

    add question preview and state props to user-page

--- src/pages/user/user-page.component.jsx
+++ src/pages/user/user-page.component.jsx
@@ -1,19 +1,43 @@
 import React from "react";
+import "./user-page.styles.scss";
 import { CustomButton } from "../../components/custom-button/custom-button.component";
 import { auth } from "../../firebase/firebas.utils";
+import QuestionPreview from "../../components/question-preview/question-preview.component";
+import { connect } from "react-redux";
+import { BackToAllButton } from "../../components/custom-button/custom-button.component";
+import { Link } from "react-router-dom";
 
-const UserPage = () => {
+const UserPage = ({ favQuestions }) => {
+  const id = "favorite";
+  const addToFav = false;
+  console.log(favQuestions);
   return (
-    <div>
-      <h2>USER PAGE</h2>
-      <CustomButton
-        onClick={() => {
-          auth.signOut();
-        }}
-      >
-        SIGN OUT
-      </CustomButton>
+    <div className="user-page">
+      <div className="nav-container">
+        <Link to={"/quiz"}>
+          <BackToAllButton />
+        </Link>
+        <CustomButton
+          onClick={() => {
+            auth.signOut();
+          }}
+        >
+          SIGN OUT
+        </CustomButton>
+      </div>
+      <div className="favorite-container">
+        <h2>Favorite questions</h2>
+        {favQuestions.questions.length ? (
+          <QuestionPreview quizId={id} addToFav={addToFav} />
+        ) : (
+          <p>No Favorite Questions Yet</p>
+        )}
+      </div>
     </div>
   );
 };
-export default UserPage;
+
+const mapStateToProps = (state) => {
+  return { favQuestions: state.quiz.questions.favorite };
+};
+export default connect(mapStateToProps)(UserPage);
