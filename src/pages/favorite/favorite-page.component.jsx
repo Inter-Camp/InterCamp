b4d0a5f10701734661c5547fc268086ed9ebc3d9
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-08 10:32:34 -0400

    favorite page component created

---
+++ src/pages/favorite/favorite-page.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./favorite-page.styles.scss";
+import QuestionPreview from "../../components/question-preview/question-preview.component";
+import { connect } from "react-redux";
+import { BackToAllButton } from "../../components/custom-button/custom-button.component";
+import { FilledStarIcon } from "../../components/icons/favorite-icons.component";
+
+const FavoritePage = ({ favQuestions }) => {
+  const id = "favorite";
+  const addToFav = false;
+  return (
+    <div className="favorite-container">
+      <div className="back-to-all-container">
+        <BackToAllButton />
+      </div>
+      <div className="fav-questions-text">
+        <FilledStarIcon />
+        <h1>Favorite questions</h1>
+        <FilledStarIcon />
+      </div>
+      {favQuestions.questions.length ? (
+        <QuestionPreview quizId={id} addToFav={addToFav} />
+      ) : (
+        <p>No Favorite Questions Yet</p>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { favQuestions: state.quiz.questions.favorite };
+};
+
+export default connect(mapStateToProps)(FavoritePage);
