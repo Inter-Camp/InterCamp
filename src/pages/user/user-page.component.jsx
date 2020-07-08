e43a1f55b94fc71d35bf623960ec0b33b71a5059
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-08 10:33:52 -0400

    moved question preview to fav page

--- src/pages/user/user-page.component.jsx
+++ src/pages/user/user-page.component.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./user-page.styles.scss";
-import { CustomButton } from "../../components/custom-button/custom-button.component";
-import { auth } from "../../firebase/firebas.utils";
-import QuestionPreview from "../../components/question-preview/question-preview.component";
-import { connect } from "react-redux";
-import { BackToAllButton } from "../../components/custom-button/custom-button.component";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebas.utils";
+
+import {
+  CustomButton,
+  BackToAllButton,
+} from "../../components/custom-button/custom-button.component";
 
 const UserPage = ({ favQuestions }) => {
-  const id = "favorite";
-  const addToFav = false;
   console.log(favQuestions);
   return (
     <div className="user-page">
       <div className="nav-container">
-        <Link to={"/quiz"}>
-          <BackToAllButton />
+        <BackToAllButton />
+        <Link className="link-to-all" to={"/favorite"}>
+          <CustomButton color="primary">Go To Favorite</CustomButton>
         </Link>
         <CustomButton
+          color="primary"
           onClick={() => {
             auth.signOut();
           }}
@@ -25,19 +26,8 @@ const UserPage = ({ favQuestions }) => {
           SIGN OUT
         </CustomButton>
       </div>
-      <div className="favorite-container">
-        <h2>Favorite questions</h2>
-        {favQuestions.questions.length ? (
-          <QuestionPreview quizId={id} addToFav={addToFav} />
-        ) : (
-          <p>No Favorite Questions Yet</p>
-        )}
-      </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return { favQuestions: state.quiz.questions.favorite };
-};
-export default connect(mapStateToProps)(UserPage);
+export default UserPage;
