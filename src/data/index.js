68d55793a72aa5394b188ab9487cf5e7027e655c
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-04-12 15:02:46 -0400

    changed id, fixed typo

--- src/data/index.js
+++ src/data/index.js
@@ -1,18 +1,18 @@
 export const ourPros = [
     {
-        id: 'oru-pro-1',
+        id: 'our-pro-1',
         imgUrl: './images/home-00.jpg',
         imgAlt: 'illustration of a woman with red hair',
         text: 'Create your own quizzes and share with friends'
     },
     {
-        id: 'oru-pro-2',
+        id: 'our-pro-2',
         imgUrl: './images/home-01.jpg',
         imgAlt: 'illustration of a man in red t-shirt',
         text: 'Get back to your favorite questions'
     },
     {
-        id: 'oru-pro-2',
+        id: 'our-pro-3',
         imgUrl: './images/home-02.jpg',
         imgAlt: 'illustration of a programmer',
         text: 'Be prepared for all types of questions'
