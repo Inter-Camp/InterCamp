84387c971c353e107f96c0c0c3448c7a8ffdeb88
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-04-11 20:51:19 -0400

    dynamic data rendering

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "./about.styles.scss";
-import Image from "../../assets/home.people.jpg";
-import ImageTwo from "../../assets/home.people.copy.jpg";
-import ImageThree from "../../assets/home.programmer.jpg";
+import { ourPros } from "../../data/index";
 
 import { CustomButton } from "../custom-button/custom-button.component";
-
 import OurProsCard from "../our-pros-card/our-pros.component";
-import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import StarsIcon from "@material-ui/icons/Stars";
-import DateRangeIcon from "@material-ui/icons/DateRange";
 
 const About = () => {
   return (
     <div className="home-container">
       <div className="home-part-one">
-        <div className="home-image-container">
-        </div>
+        <div className="home-image-container"></div>
         <div className="home-intro">
           <h1>Interview Preparation made easy</h1>
           <h3>HTML, CSS, JavaScript, React</h3>
@@ -33,24 +26,9 @@ const About = () => {
       </div>
 
       <div className="home-part-two">
-        <OurProsCard
-          image={Image}
-          icon={<AddCircleOutlineIcon color="primary" fontSize="large" />}
-        >
-          Create your own quizzes
-        </OurProsCard>
-        <OurProsCard
-          image={ImageThree}
-          icon={<StarsIcon color="secondary" fontSize="large" />}
-        >
-          Get back to your favorite questions
-        </OurProsCard>
-        <OurProsCard
-          image={ImageTwo}
-          icon={<DateRangeIcon color="primary" fontSize="large" />}
-        >
-          Be prepared
-        </OurProsCard>
+        {ourPros.map((item) => (
+          <OurProsCard image={item}>{item.text}</OurProsCard>
+        ))}
       </div>
     </div>
   );
