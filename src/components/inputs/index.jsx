aaa237be9eb58cbfc26fa87f8cdaf01f9116a8e5
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-04-15 10:55:31 -0400

    make input component required

--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./inputs.styles.scss";
 
-export const InputPrimary = ({ input, children }) => {
+export const InputPrimary = ({ input }) => {
   return (
     <input
+      required
       className="input-filled"
       type={input.type}
       id={input.id}
       placeholder={input.placeholder}
+      defaultValue=""
       name={input.id}
       autoComplete={input.autocomplete}
-    >
-      {children}
-    </input>
+    />
   );
 };
