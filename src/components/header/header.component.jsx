25b1bb83d77ec72c8b1c91d31c3c280e4ccd8dae
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-04-16 18:18:48 -0400

    changed data source in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { header } from "../../data/index";
 import Logo from "../../assets/logo.png";
-import { CustomButtonOutlined } from "../custom-button/custom-button.component";
+import { CustomButtonOutlined } from "../buttons";
 import { AccountIcon } from "../icons/account-icon.component";
 import StarFavorite from "../star-favorite/star-favorite.component";
-
-import { connect } from "react-redux";
 
 const Header = ({ currentUser }) => {
   return (
     <div className="header">
-      <Link className="logo-container" to={currentUser ? "/quiz" : "/"}>
+      <Link className="logo-container" to="/">
         <div className="logo-image-container">
           <img className="logo-image" src={Logo} alt="website logo" />
         </div>
-        <h3 className="website-name">InterCamp</h3>
+        <h3 className="website-name">{header.websiteName}</h3>
       </Link>
       <div className="button-container">
         {currentUser ? (
           <div className="user-fav-container">
             <StarFavorite />
-            <Link to={"/user"}>
+            <Link to="/user">
               <AccountIcon />
             </Link>
           </div>
         ) : (
-          <HashLink to={"/#sign-in"}>
-            <CustomButtonOutlined>SIGN IN</CustomButtonOutlined>
-          </HashLink>
+          <Link to={header.signInLink}>
+            <CustomButtonOutlined>{header.signInLabel}</CustomButtonOutlined>
+          </Link>
         )}
       </div>
     </div>
