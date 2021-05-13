a42e0d9199a441aace3ee8dfe78790f594943a45
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-05-13 16:27:10 -0400

    handle account exists in sign in options with redux

--- src/pages/sign-in-and-sign-up/sign-in-options.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-options.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { signInOptions } from "../../data/index";
 import { IButtonPrimary } from "../../components/buttons";
+import { fetchAccountExistsError } from "../../redux/user/user.actions";
+import { connect } from "react-redux";
 
 const SignInOptions = ({
   signInWithGoogle,
   signInWithFacebook,
   signInWithGitHub,
+  fetchAccountExistsError,
 }) => {
+  useEffect(() => {
+    fetchAccountExistsError();
+  });
   return (
     <section className="sign-in-options">
       <div className="sign-in-options-button-container">
@@ -15,19 +21,23 @@ const SignInOptions = ({
           onClick={signInWithGoogle}
         ></IButtonPrimary>
       </div>
-      <div className="sign-in-options-button-container disabled">
+      <div className="sign-in-options-button-container">
         <IButtonPrimary
           iconUrl={signInOptions.github.imageUrl}
-          // onClick={signInWithGitHub}
+          onClick={signInWithGitHub}
         ></IButtonPrimary>
       </div>
-      <div className="sign-in-options-button-container disabled">
+      <div className="sign-in-options-button-container">
         <IButtonPrimary
           iconUrl={signInOptions.facebook.imageUrl}
-          // onClick={signInWithFacebook}
+          onClick={signInWithFacebook}
         ></IButtonPrimary>
       </div>
     </section>
   );
 };
-export default SignInOptions;
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchAccountExistsError: () => dispatch(fetchAccountExistsError()),
+});
+export default connect(null, mapDispatchToProps)(SignInOptions);
