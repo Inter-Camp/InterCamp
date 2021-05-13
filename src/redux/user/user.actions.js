90c1a5743c4fc6946a8e9b1e8afd5329d78ea8be
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-05-13 16:25:37 -0400

    add error managment when duplicate accounts

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,35 @@
 import { UserActionTypes } from './user.types';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
     payload: user,
 });
+
+export const accountExistsError = error => ({
+    type: UserActionTypes.ACCOUNT_EXISTS_ERROR,
+    payload: error
+})
+
+export const fetchAccountExistsError = () => {
+    return dispatch => {
+        firebase.auth()
+            .getRedirectResult()
+            .then((result) => {
+                if (result.credential) {
+                    /** @type {firebase.auth.OAuthCredential} */
+                    // var credential = result.credential;
+                    // This gives you an Access Token. You can use it to access the GitHub API.
+                    // var token = credential.accessToken;
+                }
+            }).catch((error) => {
+                var email = error.email
+                firebase.auth().fetchSignInMethodsForEmail(email).then(function (methods) {
+                    const currProvider = methods[0];
+                    dispatch(accountExistsError(`There is alredy a user: ${email}, registered with ${currProvider}`))
+                });
+            });
+    }
+}
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,14 +2,20 @@ import { UserActionTypes } from './user.types';
 
 const INITIAL_STATE = {
     currentUser: null,
+    errorMsg: ''
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case UserActionTypes.SET_CURRENT_USER:
-            return{
+            return {
                 ...state,
                 currentUser: action.payload
+            }
+        case UserActionTypes.ACCOUNT_EXISTS_ERROR:
+            return {
+                ...state,
+                errorMsg: action.payload
             }
         default:
             return state;
