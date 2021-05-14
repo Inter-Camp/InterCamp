bc3da7fd911c424550c98919e0ebf1ec39039a96
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-05-13 20:47:58 -0400

    create a spinner

---
+++ src/components/spinner/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./spinner.styles.scss";
+
+const Spinner = () => (
+  <div className="spinner-overlay">
+    <div className="spinner-container" />
+  </div>
+);
+
+export default Spinner;
