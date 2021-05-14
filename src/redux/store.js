a0dc63e65174ccf34b8b686a8ec5f7bc926f5cba
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2021-05-13 20:48:15 -0400

    disable log in production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,9 @@ import rootReducer from './root-reducer';
 import thunk from 'redux-thunk';
 
 const middlewares = [thunk];
-// if(process.env.NODE_ENV === "development") {
-middlewares.push(logger);
-// }
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
